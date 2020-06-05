643d238a45edc3906335cdf322d9a87f1134e286
Daniel Ortiz <36240249+Daniel-E-Ortiz@users.noreply.github.com>
2020-06-05 11:56:34 -0500

    Add files via upload

---
+++ src/test/java/com/in28minutes/springboot/web/springDemo/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.web.springDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
